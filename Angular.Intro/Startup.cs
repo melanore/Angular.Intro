27a9e34b935f9f6416b9310f2b04c65580bca8ed
Max Grebeniuk <grebeniukmax@gmail.com>
2017-03-21 21:45:53 +0100

    Initial BK dependencies

--- Angular.Intro/Startup.cs
+++ Angular.Intro/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,7 +32,8 @@ namespace Angular.Intro
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
